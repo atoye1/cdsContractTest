bc95e731042bb3651e1f19e803957d5a9830f28d
SeolDonghun <ssalssi1@gmail.com>
2023-01-20 14:42:46 +0900

    price oracle mock filename fixed

--- contracts/Oracle/PriceConsumer.sol
+++ contracts/Oracle/PriceConsumer.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import './priceOracleMock.sol';
+import "./PriceOracleMock.sol";
 
 contract PriceConsumer {
-  PriceOracleMock private priceOracle;
+    PriceOracleMock private priceOracle;
 
-  constructor() {}
+    constructor() {}
 
-  function getPriceFromOracle() public view returns (uint256) {
-    uint256 result = priceOracle.getPrice();
-    return result;
-  }
+    function getPriceFromOracle() public view returns (uint256) {
+        uint256 result = priceOracle.getPrice();
+        return result;
+    }
 
-  function setOracle(address _priceOracleAddress) public returns (bool) {
-    require(_priceOracleAddress != address(0x0), 'Invalid address');
-    priceOracle = PriceOracleMock(_priceOracleAddress);
-    return true;
-  }
+    function setOracle(address _priceOracleAddress) public returns (bool) {
+        require(_priceOracleAddress != address(0x0), "Invalid address");
+        priceOracle = PriceOracleMock(_priceOracleAddress);
+        return true;
+    }
 }
